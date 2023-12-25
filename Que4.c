e0958228e4cecc13e6c81a1b3eac76f7b16863f4
khunt-khushi <khuntkhushi506@gmail.com>
2023-12-25 10:33:08 +0530

    Lec_8.4

---
+++ Que4.c
@@ -0,0 +1,48 @@
+/* Write a program to find the minimum and maximum and same element in a 2D array.*/
+#include<stdio.h>
+void main(){
+	
+	int i,j, n,m, max, min;
+
+	 printf("Enter size of row:- ");
+	 scanf("%d",&n);
+	 printf("Enter size of column:- ");
+	 scanf("%d",&m);
+	 
+	 	int a[n][m];
+	 	
+	for(i=0; i<n; i++){
+	for(j=0; j<m; j++){
+	
+		printf("Enter a[%d][%d]:- ",i,j);
+		scanf("%d",& a[i][j]);
+		}
+	}
+	max= a[0][0];
+	
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			if(max<a[i][j]){
+				max=a[i][j];
+			}
+		}
+	}
+
+printf("\n maximum number is :- %d",max);
+	
+	
+		min= a[0][0];
+	
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			if(min>a[i][j]){
+				min=a[i][j];
+			}
+		}
+	}
+
+printf("\n  manimum number is :- %d",min);
+	
+	
+	
+}
